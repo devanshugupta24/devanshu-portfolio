8d79ef6a3ccab8eda645a991ae7ee92f89949443
devanshugupta24 <guptadevanshu24@gmail.com>
2024-06-27 14:23:23 +0530

    footer scroll

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,53 @@
+import React from "react";
+import { Link } from "react-scroll";
 import "./Footer.css";
 
-export default function Footer(){
-return (
-<footer>
-    <div class="footerContainer">
-        <div class="socialIcons">
-            <a href="https://github.com/devanshugupta24" target="_blank"><img class="fa-brands" src="github (1).jpg"></img></a>
-            <a href="https://www.instagram.com/devanshu_2402/" target="_blank"><img class="fa-brands"src="instagram (1).jpg"></img></a>
-            <a href="https://www.linkedin.com/in/devanshu-gupta24/" target="_blank"><img class="fa-brands"src="linkedin (1).jpg"></img></a>
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footerContainer">
+        <div className="socialIcons">
+          <a href="https://github.com/devanshugupta24" target="_blank" rel="noopener noreferrer">
+            <img className="fa-brands" src="github (1).jpg" alt="GitHub" />
+          </a>
+          <a href="https://www.instagram.com/devanshu_2402/" target="_blank" rel="noopener noreferrer">
+            <img className="fa-brands" src="instagram (1).jpg" alt="Instagram" />
+          </a>
+          <a href="https://www.linkedin.com/in/devanshu-gupta24/" target="_blank" rel="noopener noreferrer">
+            <img className="fa-brands" src="linkedin (1).jpg" alt="LinkedIn" />
+          </a>
         </div>
         <div className="details">
-            <p>Devanshu Gupta &nbsp; |&nbsp; &nbsp;  guptadevanshu24@gmail.com &nbsp; |&nbsp; &nbsp; 9142088391  &nbsp; |&nbsp;&nbsp; Kaimur,Bihar</p>
-            
+          <p>
+            Devanshu Gupta &nbsp; |&nbsp; &nbsp; guptadevanshu24@gmail.com &nbsp;
+            |&nbsp; &nbsp; 9142088391 &nbsp; |&nbsp;&nbsp; Kaimur, Bihar
+          </p>
         </div>
-        <div class="footerNav">
-            <ul><li><a href="">Home</a></li>
-                <li><a href="">About Me</a></li>
-                <li><a href="">Skills</a></li>
-                <li><a href="">Projects</a></li>
-            </ul>
+        <div className="footerNav">
+          <ul>
+            <li>
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="aboutme" smooth={true} duration={500}>
+                About Me
+              </Link>
+            </li>
+            <li>
+              <Link to="skills" smooth={true} duration={500}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link to="projects" smooth={true} duration={500}>
+                Projects
+              </Link>
+            </li>
+          </ul>
         </div>
-
-        
-    </div>
-  
-</footer>
-);
+      </div>
+    </footer>
+  );
 }
